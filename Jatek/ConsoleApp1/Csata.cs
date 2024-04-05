fecc4395af78104e4cebffa88b96cbfac23ab2d6
SalaMark99 <158065747+SalaMark99@users.noreply.github.com>
2024-04-05 11:39:50 +0200

    Add files via upload

---
+++ Jatek/ConsoleApp1/Csata.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.cs
+{
+    class Csata
+    {
+        
+        public static void StartFight(Harcos harcos1,
+            Harcos harcos2)
+        {
+            
+            while (true)
+            {
+                if (GetAttackResult(harcos1, harcos2) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+
+                if (GetAttackResult(harcos2, harcos1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+        }
+
+        
+        public static string GetAttackResult(Harcos harcosA,
+            Harcos harcosB)
+        {
+            
+            double warAAttkAmt = harcosA.Attack();
+            double warBBlkAmt = harcosB.Block();
+
+            
+            double dmg2WarB = warAAttkAmt - warBBlkAmt;
+
+            
+            if (dmg2WarB > 0)
+            {
+                harcosB.Health = harcosB.Health - dmg2WarB;
+            }
+            else dmg2WarB = 0;
+
+            
+            Console.WriteLine("{0} Megtámadja {1} és bevisz {2} sebzést",
+                harcosA.Name,
+                harcosB.Name,
+                dmg2WarB);
+
+            
+            Console.WriteLine("{0} {1} életerővel rendelkezik\n",
+                harcosB.Name,
+                harcosB.Health);
+
+            
+            if (harcosB.Health <= 0)
+            {
+                Console.WriteLine("{0} Meghalt és {1} a Győztes\n",
+                    harcosB.Name,
+                    harcosA.Name);
+
+                return "Game Over";
+            }
+            else return "Fight Again";
+        }
+
+    }
+}
---
+++ Jatek/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace ConsoleApp1.cs
+{
+    class Program
+    {
+        
+ 
+        static void Main(string[] args)
+        {
+            Harcos pte = new Harcos("PTE", 1000, 120, 40);
+            Harcos me = new Harcos("MásEgyetem", 1000, 120, 40);
+ 
+            Csata.StartFight(pte, me);
+ 
+            Console.ReadLine();
+ 
+        }
+        
+    }
+ 
+}
